4c1f7367c800f4ccebe651cecb66d2bb05e94b61
SefaKahramannn <sefakahraman0001@gmail.com>
2024-12-18 22:39:18 +0300

    FooterMenu updated

--- src/test/java/pages/FooterMenu.java
+++ src/test/java/pages/FooterMenu.java
@@ -1,10 +1,16 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.GWD;
+
+import java.util.List;
 
 public class FooterMenu {
     public FooterMenu() {
         PageFactory.initElements(GWD.driver,this);
     }
+    @FindBy(css = "a[class='GMLink']")
+    public List<WebElement> footerMenu;
 }
