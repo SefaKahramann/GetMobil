528b1f217566584fd6b282474487a4dbf3ef401f
SefaKahramannn <sefakahraman0001@gmail.com>
2024-12-18 22:27:24 +0300

    DialogContent.java and TopNav.java updated

--- src/test/java/pages/DialogContent.java
+++ src/test/java/pages/DialogContent.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.GWD;
 
@@ -7,4 +9,22 @@ public class DialogContent {
     public DialogContent() {
         PageFactory.initElements(GWD.driver,this);
     }
+
+    @FindBy(xpath = "//div[text()='Siparişlerim']")
+    public WebElement orderBtn;
+
+    @FindBy(xpath = "//div[text()='Sattıklarım']")
+    public WebElement mySoldBtn;
+
+    @FindBy(xpath = "//div[text()='Beğendiklerim']")
+    public WebElement favoriteBtn;
+
+    @FindBy(xpath = "//div[text()='Kullanıcı Bilgilerim']")
+    public WebElement userDetailsBtn;
+
+    @FindBy(xpath = "//div[text()='İletişim İzinlerim']")
+    public WebElement  permissionsBtn;
+
+    @FindBy(xpath = "//div[text()='Adres Bilgilerim']")
+    public WebElement addressDetailsBtn;
 }
--- src/test/java/pages/TopNav.java
+++ src/test/java/pages/TopNav.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.GWD;
 import utilities.ReusableMethods;
 
-import java.util.ResourceBundle;
-
+import java.util.List;
 
 public class TopNav extends ReusableMethods {
     public TopNav() {
@@ -25,4 +24,31 @@ public class TopNav extends ReusableMethods {
 
     @FindBy(css = "button[data-testid='login-modal__continue-button']")
     public WebElement continueBtn;
+
+    @FindBy(xpath = "//div[text()='PROFİL']")
+    public WebElement profileBtn;
+
+    @FindBy(css = "[class='GetmobilDesktopLogoContainer']")
+    public WebElement logoBtn;
+
+    @FindBy(css = "[class='profilDropdownText']")
+    public WebElement phoneBtn;
+
+    @FindBy(css = "[data-testid='header__buyback']")
+    public WebElement buyBackBtn;
+
+    @FindBy(css = "[data-testid='header__stores']")
+    public WebElement storesBtn;
+
+    @FindBy(css = "[data-testid='header__franchises']")
+    public WebElement franchisesBtn;
+
+    @FindBy(css = "[li-type='mobilPhoneDropdownTextItem']")
+    public List<WebElement> productList;
+
+    @FindBy(css = "input[class='header-search-input']")
+    public WebElement searchBox;
+
+    @FindBy(css = "span[data-testid='search-list-phone-name']")
+    public WebElement phoneName;
 }
